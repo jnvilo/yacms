140447b29541c884781c2a65f88fd578d94d4587
Jason Viloria <jnvilo@gmail.com>
2015-03-15 13:11:03 +0100

    added exception handling to show meaningfull messages when CMSEntry objects do not exist

--- yacms/templatetags/yacms_tags.py
+++ yacms/templatetags/yacms_tags.py
@@ -6,12 +6,17 @@ from django.template.loader import get_template
 from yacms.models import CMSEntries
 register = template.Library()
 
+import logging
+logger = logging.getLogger("yacms.template_tags")
 class FrontPageArticles(template.Node):
     
     def render(self, context):
-        cmsentry_objs = CMSEntries.objects.filter(frontpage = True, published=True)
         
+        cmsentry_objs = CMSEntries.objects.filter(frontpage = True, published=True)
         #package each of the content into a YACMSViewObject
+        
+        if cmsentry_objs.count() == 0:
+            logger.debug("Warning: Found 0 FrontPage objects.")
         
         view_list = []
         for each in cmsentry_objs:
@@ -34,25 +39,37 @@ class VerticalCollapsedCategoryMenuNodes(template.Node):
         self.path = "/"
         
     def render(self, context):
-        obj = CMSEntries.objects.get(path__path = self.path)
-        obj_list = CMSEntries.objects.filter(path__parent__path=self.path, page_type__page_type="CATEGORY")
         
         html = """<select id="menu" name="menu">"""
-        html += """ <option value="/">Home</option>""".format(obj.path,obj.title)
+        html += """ <option value="/">Home</option>"""        
+        try:
+            obj = CMSEntries.objects.get(path__path = self.path)
+            obj_list = CMSEntries.objects.filter(path__parent__path=self.path, page_type__page_type="CATEGORY")
         
-        for obj in obj_list:
-            html += """ <option value="/cms{}">{}</option>""".format(obj.path.path, obj.title)
+           
             
-            #Get the children as well
-            child_obj_list = CMSEntries.objects.filter(path__parent=obj.path, page_type__page_type="CATEGORY")
-            for child_obj in child_obj_list:
-                html += """ <option value="/cms{}">&nbsp; - &nbsp;{}</option>""".format(child_obj.path.path, child_obj.title)      
+            for obj in obj_list:
+                html += """ <option value="/cms{}">{}</option>""".format(obj.path.path, obj.title)
+                
+                #Get the children as well
+                child_obj_list = CMSEntries.objects.filter(path__parent=obj.path, page_type__page_type="CATEGORY")
+                for child_obj in child_obj_list:
+                    html += """ <option value="/cms{}">&nbsp; - &nbsp;{}</option>""".format(child_obj.path.path, child_obj.title)      
+                
+                
+                
+           
+          
+        except ObjectDoesNotExist as e:
+        
+            msg = "Failed to load CMSENTRY: {} while constructing VerticalCollapsedCategoryMenuNodes".format(self.path)
+            logger.debug(msg)
             
+            html +=  """ <option value="">Failed to load: {}</option>""".format(self.path)                
+
             
-            
-        html += "</select><!--generated -->"
+        html += "</select><!--generated -->"  
         return html
-        
  
 def vertical_collapsed_menu_bar(parser, token):
    
@@ -70,25 +87,35 @@ class VerticalCategoryMenuNodes(template.Node):
         self.path = path
         
     def render(self, context):
-        obj = CMSEntries.objects.get(path__path = self.path)
-        obj_list = CMSEntries.objects.filter(path__parent__path=self.path, page_type__page_type="CATEGORY")
         
         html = """<!--generated --><ul class="nav nav-list">"""
-        html += """<li class="nav-header"><a href="/cms/{}">{}</a></li>""".format(obj.path,obj.title)
-        
-        for obj in obj_list:
-            html += """<li><a href="/cms{}" title>{}</a></li>""".format(obj.path.path, obj.title)
+        try:
+            obj = CMSEntries.objects.get(path__path = self.path)
+            obj_list = CMSEntries.objects.filter(path__parent__path=self.path, page_type__page_type="CATEGORY")
             
-            #Get the children as well
-            child_obj_list = CMSEntries.objects.filter(path__parent=obj.path, page_type__page_type="CATEGORY")
-            for child_obj in child_obj_list:
-                html += """<li><a href="/cms{}" title>&nbsp; - &nbsp;{}</a></li>""".format(child_obj.path.path, child_obj.title)            
+           
+            html += """<li class="nav-header"><a href="/cms/{}">{}</a></li>""".format(obj.path,obj.title)
             
+            for obj in obj_list:
+                html += """<li><a href="/cms{}" title>{}</a></li>""".format(obj.path.path, obj.title)
+                
+                #Get the children as well
+                child_obj_list = CMSEntries.objects.filter(path__parent=obj.path, page_type__page_type="CATEGORY")
+                for child_obj in child_obj_list:
+                    html += """<li><a href="/cms{}" title>&nbsp; - &nbsp;{}</a></li>""".format(child_obj.path.path, child_obj.title)            
+                
+                
+                
+           
+        except ObjectDoesNotExist as e:
             
+            html += "<li>Failed to load: {}</li>".format(self.path)
+            
+            msg = "Failed to load CMSEntry while building VerticalCategoryMenuNodes for path: {} ".format(self.path)
+            logger.debug("")
             
         html += "</ul><!--generated -->"
         return html
-        
  
 def vertical_menu_bar(parser, token):
     values = token.split_contents()
@@ -168,7 +195,11 @@ class CategoryMenuBar(template.Node):
                 result += "</li>"
             
             except ObjectDoesNotExist as e:
-                result += """<li>{} does not exist</li>""".format(each)
+                
+                
+                result += """<li class="dropdown">"""
+                result += """ <a href="">{}NotFound</a> """.format(each.upper().lstrip("/"))
+                result += "</li>"
             
         return result
         

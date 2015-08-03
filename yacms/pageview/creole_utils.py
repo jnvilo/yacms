14ba02dd5803b7468eb4aa749c20d61b56d02974
jason Viloria <jnvilo@gmail.com>
2015-08-03 15:11:13 -0400

    removed old repr2 compat code and string2dict and dict2string

--- yacms/pageview/creole_utils.py
+++ yacms/pageview/creole_utils.py
@@ -13,7 +13,7 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 import shlex
 
-from creole.py3compat import TEXT_TYPE, PY3, repr2
+from creole.py3compat import TEXT_TYPE, PY3
 
 try:
     from pygments import lexers
@@ -29,58 +29,6 @@ KEYWORD_MAP = {
     "False": False,
     "None": None,
 }
-
-def string2dict(raw_content, encoding="utf-8"):
-    """
-    convert a string into a dictionary. e.g.:
-
-    >>> string2dict('key1="value1" key2="value2"')
-    {'key2': 'value2', 'key1': 'value1'}
-
-    See test_creole2html.TestString2Dict()
-    """
-    if not PY3 and isinstance(raw_content, TEXT_TYPE):
-        # shlex.split doesn't work with unicode?!?
-        raw_content = raw_content.encode(encoding)
-
-    parts = shlex.split(raw_content)
-
-    result = {}
-    for part in parts:
-        key, value = part.split("=", 1)
-
-        if value in KEYWORD_MAP:
-            # True False or None
-            value = KEYWORD_MAP[value]
-        else:
-            # A number?
-            try:
-                value = int(value.strip("'\""))
-            except ValueError:
-                pass
-
-        result[key] = value
-
-    return result
-
-
-def dict2string(d):
-    """
-    FIXME: Find a better was to do this.
-
-    >>> dict2string({'foo':"bar", "no":123})
-    "foo='bar' no=123"
-
-    >>> dict2string({"foo":'bar', "no":"ABC"})
-    "foo='bar' no='ABC'"
-
-    See test_creole2html.TestDict2String()
-    """
-    attr_list = []
-    for key, value in sorted(d.items()):
-        value_string = repr2(value)
-        attr_list.append("%s=%s" % (key, value_string))
-    return " ".join(attr_list)
 
 
 def get_pygments_formatter():

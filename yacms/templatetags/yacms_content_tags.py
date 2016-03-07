44d40524f8a85c724a79b79e974782de28ee4e46
Jason Viloria <jnvilo@gmail.com>
2016-03-07 22:42:05 +0100

    work in progress

---
+++ yacms/templatetags/yacms_content_tags.py
@@ -0,0 +1,29 @@
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+from django.template import Template, Context
+from django.template.loader import get_template
+
+
+#from yacms.models import Paths, Pages
+#from yacms.pageview.base import get_pageview
+
+from yacms.models import  CMSPaths
+from yacms.models import CMSEntries
+
+register = template.Library()
+
+
+@register.inclusion_tag('yacms/templatetags/frontpage.html')
+def frontpage():
+    cmsentries = CMSEntries.objects.filter(frontpage=True, published=True)
+
+    for cmsentry in cmsentries:
+        print(cmsentry)
+    return {"cmsentries":cmsentries}
+
+
+
+
+
+
+

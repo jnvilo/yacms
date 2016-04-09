891e3680816e7ddb1691fed7270011369f18782d
Jason Viloria <jnvilo@gmail.com>
2016-04-09 23:12:05 +0200

    Added exception handling for when no CMSEntries exists to create a menu

--- yacms/templatetags/yacms_menu_tags.py
+++ yacms/templatetags/yacms_menu_tags.py
@@ -1,3 +1,5 @@
+import logging
+
 from django import template
 from django.core.exceptions import ObjectDoesNotExist
 from django.template import Template, Context
@@ -12,14 +14,28 @@ from yacms.models import CMSEntries
 
 register = template.Library()
 
+logger = logging.getLogger("yacms.templatetags")
+
+
 @register.inclusion_tag('yacms/templatetags/dropdown_menu.html')
 def dropdown_menu(path):
-    parent = CMSEntries.objects.get(path__path=path)
+
+    try:
+        parent = CMSEntries.objects.get(path__path=path)
+    except ObjectDoesNotExist as e:
+        msg = "No CMSEntries to produce dropdown_menu for path: {}".format(path)
+        logger.fatal(msg)
+        parent = []
     return {"parent":parent}
 
 @register.inclusion_tag('yacms/templatetags/full_menu.html')
 def full_menu():
-    parent = CMSEntries.objects.get(path__path="/")
+    try:
+        parent = CMSEntries.objects.get(path__path="/")
+    except ObjectDoesNotExist as e:
+        msg = "No CMSEntries to produce full_menu from /. Perhaps / does not yet exist!!"
+        logger.fatal(msg)
+        parent = None
     return {"parent":parent}
 
 

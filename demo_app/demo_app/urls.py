60c9d116ba0c553cfe4c71a9e948f27ca344ee2e
Jason Viloria <jnvilo@gmail.com>
2018-03-06 11:27:48 +0100

    started bootsrap3 bundled demo django site

---
+++ demo_app/demo_app/urls.py
@@ -0,0 +1,40 @@
+"""demo_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+
+
+from django.conf.urls import url,include
+from django.contrib import admin
+from yacms.views import CMSFrontPage
+from yacms.views import CMSLoginView
+from yacms.views import CMSLogoutView
+from yacms.views import CMSSitemap
+
+from django.contrib.sitemaps.views import sitemap
+
+sitemaps = {
+    'sitemaps': CMSSitemap,
+}
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^cms/',include('yacms.urls')),
+    url(r'^$', CMSFrontPage.as_view()),
+    url(r'^login/', CMSLoginView.as_view()),
+    url(r'^logout/', CMSLogoutView.as_view()),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
+    
+]

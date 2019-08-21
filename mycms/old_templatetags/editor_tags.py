58e37f2adb59e0dc828e659aa3909ffa47a87796
Jason Viloria <jnvilo@gmail.com>
2019-08-21 22:24:53 +0200

    cleanup unused old directories and files

--- mycms/old_templatetags/editor_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-register = template.Library()
-
-
-@register.inclusion_tag('mycms/templatetags/file_upload.html')
-def file_upload():
-    #Nothing to add to the context for now.
-    return { "none": None }

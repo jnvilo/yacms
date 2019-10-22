3c1c25656797b52fd434d545bcd917d64ef4dea4
Jason Viloria <jnvilo@gmail.com>
2019-10-22 12:55:49 +0200

    initial working get/post to page viewsets

--- mycms/api.py
+++ mycms/api.py
@@ -20,18 +20,26 @@ class PageViewSet(viewsets.ViewSet):
     This is just a standard viewset 
     """
     
-    def get_serializer(self,instance):
+    def get_serializer(self,*args, **kwargs):
         """
         The serializers are generated and stored in the PageRegistry.
-        
         This just gets the serializer from there. 
         """
         
-        #The name of a viewset is always the name + ViewSet so we can 
-        #find the name of the class by stripping away ViewSet
+        """
+        The name of a viewset is always the name + ViewSet so we can 
+        find the name of the class by stripping away ViewSet
+        """
+        
         name = self.__class__.__name__[:-7]
+        
+        """
+        The PageRegistry has the serializer built by BasePage.buildserializer()
+        and on startapp, mycms.apps.MyCMSConfig.read() calls
+        PageRegistry.get_default_serializers()
+        """
         klass =  PageRegistry.get_serializer(name)
-        return klass(instance)
+        return klass(*args, **kwargs)
     
     def get_object(self):
         """
@@ -44,7 +52,7 @@ class PageViewSet(viewsets.ViewSet):
         
         #get the CMSField Attributes. 
         
-        page = Page(self.pk)
+        page = PageClass(self.pk)
         
         #get the value of the attributes 
         return page.data
@@ -61,10 +69,25 @@ class PageViewSet(viewsets.ViewSet):
             raise APIException(detail="pk is needed")
             
         self.pk = pk
+        self.page_id = pk 
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            
+            return Response(serializer.data)
         
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        #serializer = PageSerializerBase(instance)
-        data = serializer.data
-        return Response(serializer.data)
-    
+        except Exception as e:
+            raise APIException(e)
+
+    def create(self, request, *args, **kwargs):
+        
+        print(args)
+        print(kwargs)
+        
+        print(request.data)
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response(data=serializer.data)

0637156c0bdd2d66e67b69bef28b1f1072c23f57
Jason Viloria <jnvilo@gmail.com>
2019-10-22 15:02:32 +0200

    added more custom exceptions

--- mycms/exceptions.py
+++ mycms/exceptions.py
@@ -2,7 +2,11 @@ from django.core.exceptions import ObjectDoesNotExist
 
 class NodeDoesNotExist(ObjectDoesNotExist):
     pass
+class CMSFieldDoesNotExist(ObjectDoesNotExist):
+    pass
 
 class PageDoesNotExist(Exception):
     pass
 
+class CMSFieldError(Exception):
+    pass
